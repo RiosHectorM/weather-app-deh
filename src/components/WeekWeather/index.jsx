f122d27ade66ef72ffa7e6cf2e08ec917fa3087f
RiosHectorM <104076040+RiosHectorM@users.noreply.github.com>
2023-02-02 19:20:56 +0000

    styling WeekWeather Components

--- src/components/WeekWeather/index.jsx
+++ src/components/WeekWeather/index.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import styles from "./WeekWeather.module.css";
 
 function WeekWeather() {
   return (
-    <div>WeekWeather</div>
-  )
+    <div className="flex">
+      <h3>Week Weather</h3>
+      <div>
+        <div className="flex-1">1</div>
+        <div className="flex-1">2</div>
+        <div className="flex-1">3</div>
+        <div className="flex-1">4</div>
+        <div className="flex-1">5</div>
+        <div className="flex-1">6</div>
+        <div className="flex-1">7</div>
+      </div>
+    </div>
+  );
 }
 
-export default WeekWeather
+export default WeekWeather;
