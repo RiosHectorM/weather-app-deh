506473d0dd2015cf77098eff2ad96641354202fc
RiosHectorM <rios.hector.martin@gmail.com>
2023-02-07 07:37:28 -0300

    "deploy now"

--- src/components/DataWeather/index.jsx
+++ src/components/DataWeather/index.jsx
@@ -15,26 +15,18 @@ function DataWeather({ humidity, pressure, visibility, speed }) {
           className={`sm:flex sm:flex-row sm:flex-nowrap h-full overflow-auto rounded-3xl bg-black bg-opacity-70 py-2 mx-10 md:mx-20 lg:mx-32 text-white justify-around text-center grid gap-4 grid-cols-2`}
         >
           <div className="flex flex-col items-center">
-            <img
-              src="../../../public/agua.png"
-              alt="humidity"
-              className="w-auto h-[100px]"
-            />
+            <img src="/agua.png" alt="humidity" className="w-auto h-[100px]" />
             <p className="self-center px-2">Humidity </p>
             <p className="text-2xl">{humidity}%</p>
           </div>
           <div className="flex flex-col items-center">
-            <img
-              src="../../../public/press.png"
-              alt="pressure"
-              className="w-auto h-[100px]"
-            />
+            <img src="/press.png" alt="pressure" className="w-auto h-[100px]" />
             <p className="self-center px-2">Pressure </p>
             <p className="text-2xl">{pressure} hPa</p>
           </div>
           <div className="flex flex-col items-center">
             <img
-              src="../../../public/windsock.png"
+              src="/windsock.png"
               alt="windspeed"
               className="w-auto h-[100px]"
             />
@@ -43,7 +35,7 @@ function DataWeather({ humidity, pressure, visibility, speed }) {
           </div>
           <div className="flex flex-col items-center">
             <img
-              src="../../../public/visibility.png"
+              src="/visibility.png"
               alt="visibility"
               className="w-auto h-[100px]"
             />
